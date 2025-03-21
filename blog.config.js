b6b9442d7c2be65319c838d4c3a247b8a662e6cd
AronYang <impixelhu@gmail.com>
2025-03-21 15:48:51 +0800

    add comments

--- blog.config.js
+++ blog.config.js
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: "", // leave it empty if you don't need any comment plugin
+    provider: "cusdis", // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: "", // The repository of store comments
       owner: "",
@@ -51,7 +51,7 @@ const BLOG = {
       repo: "",
     },
     cusdisConfig: {
-      appId: "", // data-app-id
+      appId: "cb3a445e-c5e6-4fa2-9ba6-fad7c1e9854f", // data-app-id
       host: "https://cusdis.com", // data-host, change this if you're using self-hosted version
       scriptSrc: "https://cusdis.com/js/cusdis.es.js", // change this if you're using self-hosted version
     },
