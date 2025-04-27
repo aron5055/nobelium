876020c7b130ce0d4bb20cc8e5c56d2f18ec7e3d
Aron Yang <impixelhu@gmail.com>
2025-04-27 19:20:37 +0800

    update description

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: "Aron Yang",
   email: "impixelhu@gmail.com",
   link: "https://blog.aronyang.com",
-  description: "我的价值来源于人生的痛苦",
+  description: "I love Alice.",
   lang: "zh-CN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: "Asia/Shanghai", // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: "light", // ['light', 'dark', 'auto'],
